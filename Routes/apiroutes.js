865a1cb5bfd24071cd1a9dfdea5fd1bfcb479633
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-07-19 19:06:37 -0500

    .post added to apiroutes

--- Routes/apiroutes.js
+++ Routes/apiroutes.js
@@ -7,3 +7,13 @@ router.get('/notes', (req, res) => {
     const notes = getNotes();
     res.json(notes);
 });
+
+router.post('/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = getNotes();
+    newNote.id = generateNoteId(notes);
+    notes.push(newNote);
+    saveNotes(notes);
+    res.json(newNote);
+});
+
